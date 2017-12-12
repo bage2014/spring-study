27e8388fd75d5d393a0117ba06be5ddc376b2e57
bage <bage@192.168.57.2>
2017-12-12 22:39:15 +0800

    测试提交

---
+++ spring-study/spring-study-beans/src/main/java/com/bage/javaconfig/eventhandling/EmailService.java
@@ -0,0 +1,17 @@
+package com.bage.javaconfig.eventhandling;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+
+public class EmailService implements ApplicationEventPublisherAware{
+
+	private ApplicationEventPublisher applicationEventPublisher;
+	
+	public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+		this.applicationEventPublisher = applicationEventPublisher;
+	}
+
+	
+	
+	
+}
