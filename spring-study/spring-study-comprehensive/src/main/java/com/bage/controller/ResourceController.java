b7c17ae8db20f799d0d38ea3dadd922fef98641b
bage <bage@192.168.57.2>
2017-12-19 21:31:02 +0800

    增加对resource的使用集成

--- spring-study/spring-study-comprehensive/src/main/java/com/bage/controller/ResourceController.java
+++ spring-study/spring-study-comprehensive/src/main/java/com/bage/controller/ResourceController.java
@@ -1,24 +1,45 @@
 package com.bage.controller;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.bage.domain.HsqldbDataSource;
 
 @Controller
 @RequestMapping("resource")
 public class ResourceController {
 	
+	@Autowired
+	private HsqldbDataSource hsqldbDataSource;
+	
+	@Autowired
+	private Environment environment;
+	
+	@Value("${jdbc.url}")
+	private String url ;
+	
 	@RequestMapping("properties")
 	@ResponseBody
-	public String hello(HttpServletRequest request){
-		ServletContext sc = request.getSession().getServletContext(); 
-		ApplicationContext cnt = WebApplicationContextUtils.getWebApplicationContext(sc );
-		return cnt.getEnvironment().getProperty("jdbc.url") + "," + cnt.getBean("xmlBeanForImport");
+	public String properties(HttpServletRequest request){
+		return url;
+	}
+
+	@RequestMapping("environment")
+	@ResponseBody
+	public String environment(HttpServletRequest request){
+		return environment.getProperty("jdbc.url", "defaultValue");
+	}
+	
+	@RequestMapping("hsqldbDataSource")
+	@ResponseBody
+	public String hsqldbDataSource(HttpServletRequest request){
+		return hsqldbDataSource + "";
 	}
 	
 }
